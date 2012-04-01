c4bed727fcef5418e2514dfff43768cb1d74bb35
landlessness <brian@landlessness.net>
2012-04-01 04:14:40 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saladshooter::Application.config.secret_token = '754a59d155dba16c9fa08978f6162df52e2297dc101775093324d83f81f24301874ff4a8a221f998b93be9ea9715db38cac59774f3ef45965308e40f5e4508a5'
